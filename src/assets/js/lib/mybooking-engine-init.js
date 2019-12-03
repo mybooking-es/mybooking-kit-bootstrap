3e44d44dfae5852fa664bd8549ed395623b5b724
Marcel Reig <marcelreig@gmail.com>
2019-12-03 14:02:30 +0100

    order + engine google api

--- src/assets/js/lib/mybooking-engine-init.js
+++ src/assets/js/lib/mybooking-engine-init.js
@@ -1,10 +1,22 @@
 window.mybookingEngine = (function() {
-  var baseURL = "https://demo-rentacar-dev.mybooking.es";
+  var baseURL = "";
   var extrasStep = false;
   var chooseProductUrl = "choose_product.html";
   var chooseExtrasUrl = "choose_extras.html";
   var completeUrl = "complete.html";
   var summaryUrl = "summary.html";
+  var useGoogleMaps = true;
+  var googleMapsSettings = {
+    apiKey: "",
+    settings: {
+      googleMapsRestrictCountryCode: "es",
+      googlePlacesRetrictBounds: true,
+      googleMapsBoundsSWLat: 40.2652276,
+      googleMapsBoundsSWLng: -3.9374601,
+      googleMapsBoundsNELat: 40.5689585,
+      googleMapsBoundsNELng: -3.4856473
+    }
+  };
   function getBaseURL() {
     return baseURL;
   }
@@ -23,9 +35,17 @@ window.mybookingEngine = (function() {
   function getSummaryUrl() {
     return summaryUrl;
   }
+  function getUseGoogleMaps() {
+    return useGoogleMaps;
+  }
+  function getGoogleMapsSettings() {
+    return googleMapsSettings;
+  }
   return {
     baseURL: getBaseURL,
     extrasStep: getExtrasStep,
+    useGoogleMaps: getUseGoogleMaps,
+    googleMapsSettings: getGoogleMapsSettings,
     chooseProductUrl: getChooseProductUrl,
     chooseExtrasUrl: getChooseExtrasUrl,
     completeUrl: getCompleteUrl,
