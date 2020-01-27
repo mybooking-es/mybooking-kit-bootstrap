413fe4fe13be3152282f42c75c413f13010c6c80
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-01-27 17:09:52 +0100

    engine review

---
+++ dist/assets/js/mybooking-engine-init-activities.js
@@ -0,0 +1,24 @@
+window.mybookingEngine = (function() {
+  var baseURL = "https://demo-tours.mybooking.es";
+  var apiKey = "";
+  var shoppingCartUrl = "activity-shopping-cart.html";
+  var orderUrl = "order.html";
+  function getBaseURL() {
+    return baseURL;
+  }
+  function getApiKey() {
+    return apiKey;
+  }
+  function getShoppingCartUrl() {
+    return shoppingCartUrl;
+  }
+  function getOrderUrl() {
+    return orderUrl;
+  }
+  return {
+    baseURL: getBaseURL,
+    apiKey: getApiKey,
+    shoppingCartUrl: getShoppingCartUrl,
+    orderUrl: getOrderUrl
+  };
+})();
