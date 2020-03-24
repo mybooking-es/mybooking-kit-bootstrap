b70707e0609b779638f670de89ff92da1dd6d38e
Marcel Reig <marcelreig@gmail.com>
2020-03-24 19:39:12 +0100

    add new css for selector select labels

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -84,6 +84,44 @@ jQuery(document).ready(function($) {
   });
 });
 
+// Slide fleet
+$(document).ready(function() {
+  $(".fleet_slide").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
 // Nav Language selector
 jQuery(document).ready(function($) {
   if ($("html").attr("lang") == "es") {
